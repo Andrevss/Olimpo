034ebb0445245269c0b77f66d982581772714b7b
André Silva <andre_vss@outlook.com>
2025-08-26 16:58:43 -0300

    adjusting the readme and finishin the website

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -12,8 +12,8 @@ const Footer = () => {
 
     return (
         <footer className='bg-[#1C1C1C]'>
-            <div className='w-[85%] flex justify-between mx-auto pb-12 pt-10 md-lg:pb-10 sm:pb-6 md:flex-col'>
-                    <div className='text-[#E0E0E0] font-grotesk flex-col sm:justify-start sm:mt-6'>
+            <div className='w-[85%] flex justify-between mx-auto pb-12 pt-10 md-lg:pb-10 sm:pb-4 md:flex-col'>
+                    <section className='text-[#E0E0E0] font-grotesk flex-col sm:justify-start sm:mt-4'>
                         <h2 className='text-lg'>Formas de Pagamento</h2>
                         <div className='flex gap-4 text-[#E0E0E0] mt-2 text-2xl'>
                             <span href="###" ><FaCcMastercard /></span>
@@ -21,25 +21,24 @@ const Footer = () => {
                             <span href="###" ><FaPix /></span>
                         </div>
                         <h6 className='mt-3 text-xs	'>*Consulte nossos termos para informações sobre frete</h6>
-                    </div>
-                <section className=''>
-                    <div className='text-[#E0E0E0] font-grotesk flex-col justify-center sm:justify-start sm:mt-6 '>
+                    </section>
+                
+                    <section className='text-[#E0E0E0] font-grotesk flex-col justify-center sm:justify-start sm:mt-4 '>
                         <h2 className='text-lg'>Nos siga nas redes sociais</h2>
                         <div className='flex gap-4 text-[#E0E0E0] mt-2 text-xl'>
                             <a href="https://www.instagram.com/olimpo081/?hl=pt-br" target="_blank" rel="noopener noreferrer" className="text-[#E0E0E0] hover:text-[#F2A541]"><FaInstagram /></a>
                             <a href="https://www.tiktok.com/@olimpo081pe?_t=ZM-8zCSydCmi86&_r=1" target="_blank" rel="noopener noreferrer" className="text-[#E0E0E0] hover:text-[#F2A541]"><FaTiktok /></a>
                         </div>
-                    </div>
-                </section>
-                <section className=''>
-                    <div className='text-[#E0E0E0] font-grotesk flex-col justify-center sm:justify-start sm:mt-6'>
+                    </section>
+            
+                    <section className='text-[#E0E0E0] font-grotesk flex-col justify-center sm:justify-start sm:mt-4'>
                         <h2 className='text-lg'>Fale Conosco</h2>
                         <div className='flex gap-4 text-[#E0E0E0] mt-2 text-xl'>
-                            <a href="###" className="text-[#E0E0E0] hover:text-[#F2A541]"><FaWhatsapp /></a>
-                            <a href="###" className="text-[#E0E0E0] hover:text-[#F2A541]"><MdEmail /></a>
+                            <a href="https://wa.me/558197146120?text=Olá! Gostaria de mais informações sobre alguns produtos." target="_blank" rel="noopener noreferrer" className="text-[#E0E0E0] hover:text-[#F2A541]"><FaWhatsapp /></a>
+                            <a href="mailto:olimpoPE081@outlook.com?subject=Contato pelo site&body=Olá, gostaria de mais informações sobre os produtos." className="text-[#E0E0E0] hover:text-[#F2A541]"><MdEmail /></a>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                
             </div>
             <div className='w-[90%] flex flex-wrap justify-center items-center text-[#F2A541] mx-auto pb-4 text-center text-sm font-grotesk'>
                 <span>Copyright @2025 All Rights Reserved</span>
